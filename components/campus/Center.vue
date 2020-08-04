28729c1a25134833a8483c9be9982c3ecd37cffe
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-08-04 02:42:48 +0200

    add 2 mores components

---
+++ components/campus/Center.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  v-row(justify="center")
+    v-col(cols="12", md="8", lg="6", xl="4", align="center")
+      slot
+</template>
+
+<script>
+export default {
+  name: 'Center',
+}
+</script>
