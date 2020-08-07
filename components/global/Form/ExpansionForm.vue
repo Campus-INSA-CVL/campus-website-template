a7ca701f54ab07c90a001bdf18034cba52f6b54d
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-08-08 01:00:45 +0200

    add generator: expansion

--- components/global/Form/ExpansionForm.vue
+++ components/global/Form/ExpansionForm.vue
@@ -1,15 +1,16 @@
 <template lang="pug">
   v-row(tag="section", no-gutters)
     v-col
-      h2.text-h6.font-weight-bold Vos réseaux sociaux
+      h2.text-h6.font-weight-bold Vos panneaux extensibles
       v-row(justify="center")
         v-col(v-for="(component, indice) in value", :key="indice", cols="12")
           v-text-field(v-if="is('text-field', component.type)", :label="component.label", v-model="component.value", :key="indice", outlined, hide-details)
+          v-textarea(v-else-if="is('text-area', component.type)", :label="component.label", v-model="component.value", :key="indice", outlined, hide-details)
 </template>
 
 <script>
 export default {
-  name: 'SocialForm',
+  name: 'ExpansionForm',
   props: {
     value: {
       type: Array,
